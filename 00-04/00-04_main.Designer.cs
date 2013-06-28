8b9b8187cdbc571f2ca11d1f8da33f2a441f5066
MasuqaT <occar@hotmail.co.jp>
2013-06-29 00:57:33 +0900

    00-04::WindowsForm
    00-04

---
+++ 00-04/00-04_main.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace OpenTK_Sample
+{
+	partial class WinForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.glControl = new OpenTK.GLControl();
+			this.SuspendLayout();
+			// 
+			// glControl
+			// 
+			this.glControl.BackColor = System.Drawing.Color.Black;
+			this.glControl.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.glControl.Location = new System.Drawing.Point(0, 0);
+			this.glControl.Name = "glControl";
+			this.glControl.Size = new System.Drawing.Size(784, 562);
+			this.glControl.TabIndex = 0;
+			this.glControl.VSync = false;
+			this.glControl.Load += new System.EventHandler(this.glControl_Load);
+			this.glControl.Paint += new System.Windows.Forms.PaintEventHandler(this.glControl_Paint);
+			this.glControl.Resize += new System.EventHandler(this.glControl_Resize);
+			// 
+			// WinForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.Control;
+			this.ClientSize = new System.Drawing.Size(784, 562);
+			this.Controls.Add(this.glControl);
+			this.Name = "WinForm";
+			this.Text = "0-4:WindowsForm";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private OpenTK.GLControl glControl;
+
+
+	}
+}
---
+++ 00-04/00-04_main.cs
@@ -0,0 +1,70 @@
+﻿/**
+ * 参照設定 : OpenTK OpenTK.GLControl System System.Data System.Drawing System.Windows.Forms System.Xml
+ */
+
+using System;
+using System.Windows.Forms;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+namespace OpenTK_Sample
+{
+	public partial class WinForm : Form
+	{
+		public WinForm()
+		{
+			InitializeComponent();
+		}
+
+		private void glControl_Load(object sender, EventArgs e)
+		{
+			GL.ClearColor(Color4.Black);
+			GL.Enable(EnableCap.DepthTest);
+		}
+
+		private void glControl_Resize(object sender, EventArgs e)
+		{
+			GL.Viewport(ClientRectangle.X, ClientRectangle.Y, ClientRectangle.Width, ClientRectangle.Height);
+			GL.MatrixMode(MatrixMode.Projection);
+			Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView((float)Math.PI / 4, (float)Width / (float)Height, 1.0f, 64.0f);
+			GL.LoadMatrix(ref projection);
+		}
+
+		private void glControl_Paint(object sender, PaintEventArgs e)
+		{
+			GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+			GL.MatrixMode(MatrixMode.Modelview);
+			Matrix4 modelview = Matrix4.LookAt(Vector3.Zero, Vector3.UnitZ, Vector3.UnitY);
+			GL.LoadMatrix(ref modelview);
+
+			GL.Begin(BeginMode.Quads);
+
+			GL.Color4(Color4.White);
+			GL.Vertex3(-1.0f, 1.0f, 4.0f);
+			GL.Color4(Color4.Red);
+			GL.Vertex3(-1.0f, -1.0f, 4.0f);
+			GL.Color4(Color4.Lime);
+			GL.Vertex3(1.0f, -1.0f, 4.0f);
+			GL.Color4(Color4.Blue);
+			GL.Vertex3(1.0f, 1.0f, 4.0f);
+
+			GL.End();
+			glControl.SwapBuffers();
+		}
+	}
+
+	class OpenTK_Sample
+	{
+		[STAThread]
+		static int Main()
+		{
+			using (WinForm window = new WinForm())
+			{
+				window.ShowDialog();
+			}
+			return 0;
+		}
+	}
+}

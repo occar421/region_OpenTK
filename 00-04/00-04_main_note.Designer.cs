2298a2b7794751066bf4e032194ac3b16bff4f39
MasuqaT <occar@hotmail.co.jp>
2013-06-30 20:20:29 +0900

    00-04::WindowsForm note
    advanced

---
+++ 00-04/00-04_main_note.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace OpenTK_Sample
+{
+	partial class WinForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.glControl1 = new OpenTK.GLControl();
+			this.glControl2 = new OpenTK.GLControl();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+			this.splitter = new System.Windows.Forms.Splitter();
+			this.panel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			this.panel2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// glControl1
+			// 
+			this.glControl1.BackColor = System.Drawing.Color.Black;
+			this.glControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.glControl1.Location = new System.Drawing.Point(0, 0);
+			this.glControl1.Name = "glControl1";
+			this.glControl1.Size = new System.Drawing.Size(395, 562);
+			this.glControl1.TabIndex = 0;
+			this.glControl1.VSync = false;
+			this.glControl1.Load += new System.EventHandler(this.glControl1_Load);
+			this.glControl1.Resize += new System.EventHandler(this.glControl1_Resize);
+			// 
+			// glControl2
+			// 
+			this.glControl2.BackColor = System.Drawing.Color.Black;
+			this.glControl2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.glControl2.Location = new System.Drawing.Point(0, 0);
+			this.glControl2.Name = "glControl2";
+			this.glControl2.Size = new System.Drawing.Size(389, 562);
+			this.glControl2.TabIndex = 1;
+			this.glControl2.VSync = false;
+			this.glControl2.Load += new System.EventHandler(this.glControl2_Load);
+			this.glControl2.Resize += new System.EventHandler(this.glControl2_Resize);
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.numericUpDown1);
+			this.panel1.Controls.Add(this.glControl1);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(395, 562);
+			this.panel1.TabIndex = 2;
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.numericUpDown1.Location = new System.Drawing.Point(0, 543);
+			this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            -2147483648});
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(395, 19);
+			this.numericUpDown1.TabIndex = 1;
+			// 
+			// panel2
+			// 
+			this.panel2.Controls.Add(this.numericUpDown2);
+			this.panel2.Controls.Add(this.glControl2);
+			this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel2.Location = new System.Drawing.Point(395, 0);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(389, 562);
+			this.panel2.TabIndex = 3;
+			// 
+			// numericUpDown2
+			// 
+			this.numericUpDown2.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.numericUpDown2.Location = new System.Drawing.Point(0, 543);
+			this.numericUpDown2.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+			this.numericUpDown2.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            -2147483648});
+			this.numericUpDown2.Name = "numericUpDown2";
+			this.numericUpDown2.Size = new System.Drawing.Size(389, 19);
+			this.numericUpDown2.TabIndex = 2;
+			// 
+			// splitter
+			// 
+			this.splitter.Location = new System.Drawing.Point(395, 0);
+			this.splitter.Name = "splitter";
+			this.splitter.Size = new System.Drawing.Size(5, 562);
+			this.splitter.TabIndex = 4;
+			this.splitter.TabStop = false;
+			// 
+			// WinForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.Control;
+			this.ClientSize = new System.Drawing.Size(784, 562);
+			this.Controls.Add(this.splitter);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.panel1);
+			this.Name = "WinForm";
+			this.Text = "0-4:WindowsForm";
+			this.panel1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			this.panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private OpenTK.GLControl glControl1;
+		private OpenTK.GLControl glControl2;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.NumericUpDown numericUpDown2;
+		private System.Windows.Forms.Splitter splitter;
+
+
+	}
+}
---
+++ 00-04/00-04_main_note.cs
@@ -0,0 +1,157 @@
+﻿/**
+ * 参照設定 : OpenTK OpenTK.GLControl System System.Drawing System.Windows.Forms
+ */
+
+using System;
+using System.Windows.Forms;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+namespace OpenTK_Sample
+{
+	public partial class WinForm : Form
+	{
+		//回転角を保持する変数
+		float deg1 = 0.0f;
+		float deg2 = 0.0f;
+		public WinForm()
+		{
+			//(デザイナで指定)800x600のウィンドウを作る。タイトルは「0-4:WindowsForm」
+			InitializeComponent();
+			//プログラムが暇なときに動作する
+			Application.Idle += (sender, e) =>
+			{
+				//2つのコントロールがアイドル状態なら両方描画
+				while (glControl1.IsIdle && glControl2.IsIdle)
+				{
+					Render1();
+
+					deg1 += (float)numericUpDown1.Value;
+					if (deg1 > 360.0f)
+						deg1 -= 360.0f;
+					if (deg1 < 0.0f)
+						deg1 += 360.0f;
+
+					Render2();
+
+					deg2 += (float)numericUpDown2.Value;
+					if (deg2 > 360.0f)
+						deg2 -= 360.0f;
+					if (deg2 < 0.0f)
+						deg2 += 360.0f;
+				}
+			};
+		}
+
+		//glControl1の起動時に実行される。
+		private void glControl1_Load(object sender, EventArgs e)
+		{
+			glControl1.MakeCurrent();	//OpenTKの処理先をglControl1に変更
+
+			GL.ClearColor(Color4.Black);
+			GL.Enable(EnableCap.DepthTest);
+		}
+
+		//glControl1のサイズ変更時に実行される。
+		private void glControl1_Resize(object sender, EventArgs e)
+		{
+			glControl1.MakeCurrent();	//OpenTKの処理先をglControl1に変更
+
+			GL.Viewport(0, 0, glControl1.Size.Width, glControl1.Size.Height);
+			GL.MatrixMode(MatrixMode.Projection);
+			Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView((float)Math.PI / 4, (float)glControl1.Size.Width / (float)glControl1.Size.Height, 1.0f, 64.0f);
+			GL.LoadMatrix(ref projection);
+		}
+
+		//glControl1の描画関数
+		private void Render1()
+		{
+			glControl1.MakeCurrent();	//OpenTKの処理先をglControl1に変更
+
+			GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+			GL.MatrixMode(MatrixMode.Modelview);
+			Matrix4 modelview = Matrix4.LookAt(Vector3.Zero, Vector3.UnitZ, Vector3.UnitY);
+			GL.LoadMatrix(ref modelview);
+
+			GL.Rotate(deg1, Vector3.UnitZ);
+
+			GL.Begin(BeginMode.Quads);
+
+			GL.Color4(Color4.White);
+			GL.Vertex3(-1.0f, 1.0f, 4.0f);
+			GL.Color4(Color4.Red);
+			GL.Vertex3(-1.0f, -1.0f, 4.0f);
+			GL.Color4(Color4.Lime);
+			GL.Vertex3(1.0f, -1.0f, 4.0f);
+			GL.Color4(Color4.Blue);
+			GL.Vertex3(1.0f, 1.0f, 4.0f);
+
+			GL.End();
+			glControl1.SwapBuffers();
+		}
+
+		//glControl2の起動時に実行される
+		private void glControl2_Load(object sender, EventArgs e)
+		{
+			glControl2.MakeCurrent();	//OpenTKの処理先をglControl2に変更
+
+			GL.ClearColor(Color4.Black);
+			GL.Enable(EnableCap.DepthTest);
+		}
+
+		//glControl2のサイズ変更時に実行される
+		private void glControl2_Resize(object sender, EventArgs e)
+		{
+			glControl2.MakeCurrent();	//OpenTKの処理先をglControl2に変更
+
+			GL.Viewport(0, 0, glControl2.Size.Width, glControl2.Size.Height);
+			GL.MatrixMode(MatrixMode.Projection);
+			Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView((float)Math.PI / 4, (float)glControl2.Size.Width / (float)glControl2.Size.Height, 1.0f, 64.0f);
+			GL.LoadMatrix(ref projection);
+
+		}
+
+		//glControl2の描画関数
+		private void Render2()
+		{
+			glControl2.MakeCurrent();	//OpenTKの処理先をglControl2に変更
+
+			GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+			GL.MatrixMode(MatrixMode.Modelview);
+			Matrix4 modelview = Matrix4.LookAt(Vector3.Zero, Vector3.UnitZ, Vector3.UnitY);
+			GL.LoadMatrix(ref modelview);
+
+			GL.Rotate(deg2, Vector3.UnitZ);
+
+			GL.Begin(BeginMode.Quads);
+
+			GL.Color4(Color4.White);
+			GL.Vertex3(-1.0f, 1.0f, 4.0f);
+			GL.Color4(Color4.Red);
+			GL.Vertex3(-1.0f, -1.0f, 4.0f);
+			GL.Color4(Color4.Lime);
+			GL.Vertex3(1.0f, -1.0f, 4.0f);
+			GL.Color4(Color4.Blue);
+			GL.Vertex3(1.0f, 1.0f, 4.0f);
+
+			GL.End();
+			glControl2.SwapBuffers();
+		}
+	}
+
+	class OpenTK_Sample
+	{
+		[STAThread]
+		static int Main()
+		{
+			using (WinForm window = new WinForm())
+			{
+				window.ShowDialog();
+			}
+			return 0;
+		}
+	}
+}

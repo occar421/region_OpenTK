05e9f0b5d85edcc694ef6cf2604820cb494dfcbc
MasuqaT <occar@hotmail.co.jp>
2013-07-10 19:13:07 +0900

    01-02::Mouse Tracking View
    Viewer of rotatable, colorful pyramid.

---
+++ 01-02/01-02_main.cs
@@ -0,0 +1,224 @@
+﻿/*
+ * 参照設定 : OpenTK System.Drawing
+ */
+
+using System;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+
+namespace OpenTK_Samples
+{
+	class OpenTK_Sample
+	{
+		[STAThread]
+		static int Main()
+		{
+			using (Game window = new Game())
+			{
+				window.Run(30.0);
+			}
+			return 0;
+		}
+	}
+
+	class Game : GameWindow
+	{
+		bool isTracking;			//トラッキング中かどうか
+		Vector2 current, previous;	//現在の点、前の点
+		Matrix4 rotate;				//回転行列
+		float zoom;					//拡大度
+		float wheelPrevious;		//マウスホイールの前の状態
+
+		//800x600のウィンドウを作る。タイトルは「1-2:Mouse Tracking View」
+		public Game()
+			: base(800, 600, GraphicsMode.Default, "1-2:Mouse Tracking View")
+		{
+			VSync = VSyncMode.On;
+
+			isTracking = false;
+			current = Vector2.Zero;
+			previous = Vector2.Zero;
+			rotate = Matrix4.Identity;
+			zoom = 1.0f;
+			wheelPrevious = 0.0f;
+
+			#region MouseTracking
+
+			//マウスボタンが押されると発生するイベント
+			this.Mouse.ButtonDown += (sender, e) =>
+			{
+				//右ボタンが押された場合
+				if (e.Button == MouseButton.Right)
+				{
+					isTracking = true;
+					current = new Vector2(this.Mouse.X, this.Mouse.Y);
+				}
+			};
+			
+			//マウスボタンが離されると発生するイベント
+			this.Mouse.ButtonUp += (sender, e) =>
+			{
+				//右ボタンが押された場合
+				if (e.Button == MouseButton.Right)
+				{
+					isTracking = false;
+					previous = Vector2.Zero;
+				}
+			};
+			
+			//マウスが動くと発生するイベント
+			this.Mouse.Move += (sender, e) =>
+			{
+				//トラッキング中の場合
+				if (isTracking)
+				{
+					previous = current;
+					current = new Vector2(this.Mouse.X, this.Mouse.Y);
+					Vector2 delta = current - previous;
+					delta /= (float)Math.Sqrt(this.Width * this.Width + this.Height * this.Height);
+					float length = delta.Length;
+					if (length > 0.0)
+					{
+						float rad = length * (float)Math.PI;
+						float theta = (float)Math.Sin(rad) / length;
+						Quaternion after = new Quaternion(delta.Y * theta, delta.X * theta, 0.0f, (float)Math.Cos(rad));
+						rotate = rotate * Matrix4.Rotate(after);
+					}
+				}
+			};
+
+			//マウスホイールが回転すると発生するイベント
+			this.Mouse.WheelChanged += (sender, e) =>
+			{
+				float delta = (float)this.Mouse.Wheel - wheelPrevious;
+
+				zoom *= (float)Math.Pow(1.2, delta);
+
+				//拡大、縮小の制限
+				if (zoom > 2.5f)
+					zoom = 2.5f;
+				if (zoom < 0.4f)
+					zoom = 0.4f;
+				wheelPrevious = this.Mouse.Wheel;
+			};
+			
+			#endregion
+		}
+
+		//ウィンドウの起動時に実行される。
+		protected override void OnLoad(EventArgs e)
+		{
+			base.OnLoad(e);
+
+			GL.ClearColor(Color4.Black);
+			GL.Enable(EnableCap.DepthTest);
+		}
+
+		//ウィンドウのサイズが変更された場合に実行される。
+		protected override void OnResize(EventArgs e)
+		{
+			base.OnResize(e);
+
+			GL.Viewport(ClientRectangle);
+
+			Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView((float)Math.PI / 4, Width / (float)Height, 1.0f, 64.0f);
+			GL.MatrixMode(MatrixMode.Projection);
+			GL.LoadMatrix(ref projection);
+		}
+
+		//画面更新で実行される。
+		protected override void OnUpdateFrame(FrameEventArgs e)
+		{
+			base.OnUpdateFrame(e);
+
+			//Escapeキーで終了
+			if (Keyboard[Key.Escape])
+			{
+				this.Exit();
+			}
+
+			//F1キーで回転をリセット
+			if (Keyboard[Key.F1])
+			{
+				rotate = Matrix4.Identity;
+			}
+
+			//F2キーでY軸90度回転
+			if (Keyboard[Key.F2])
+			{
+				rotate = Matrix4.CreateRotationY((float)Math.PI / 2.0f);
+			}
+
+			//F3キーでY軸180度回転
+			if (Keyboard[Key.F3])
+			{
+				rotate = Matrix4.CreateRotationY((float)Math.PI);
+			}
+
+			//F4キーでY軸270度回転
+			if (Keyboard[Key.F4])
+			{
+				rotate = Matrix4.CreateRotationY(3.0f * (float)Math.PI / 2.0f);
+			}
+
+			//F5キーで拡大をリセット
+			if(Keyboard[Key.F5])
+			{
+				zoom=1.0f;
+			}
+		}
+
+		//画面描画で実行される。
+		protected override void OnRenderFrame(FrameEventArgs e)
+		{
+			base.OnRenderFrame(e);
+
+			GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+			Matrix4 modelview = Matrix4.LookAt(Vector3.UnitZ * 10, Vector3.Zero, Vector3.UnitY);
+			GL.MatrixMode(MatrixMode.Modelview);
+			GL.LoadMatrix(ref modelview);
+			GL.MultMatrix(ref rotate);
+			Matrix4 scale = Matrix4.Scale(zoom);
+			GL.MultMatrix(ref scale);
+
+			drawPyramid();
+
+			SwapBuffers();
+		}
+
+		//正四角錐を描画する。
+		private void drawPyramid()
+		{
+			GL.Begin(BeginMode.Triangles);
+
+			GL.Color4(Color4.Coral);
+			GL.Vertex3(1.0f, 1.0f, -1.0f);
+			GL.Vertex3(1.0f, -1.0f, -1.0f);
+			GL.Vertex3(0.0f, 0.0f, 1.0f);
+			GL.Color4(Color4.Navy);
+			GL.Vertex3(1.0f, -1.0f, -1.0f);
+			GL.Vertex3(-1.0f, -1.0f, -1.0f);
+			GL.Vertex3(0.0f, 0.0f, 1.0f);
+			GL.Color4(Color4.Green);
+			GL.Vertex3(-1.0f, -1.0f, -1.0f);
+			GL.Vertex3(-1.0f, 1.0f, -1.0f);
+			GL.Vertex3(0.0f, 0.0f, 1.0f);
+			GL.Color4(Color4.LightSkyBlue);
+			GL.Vertex3(-1.0f, 1.0f, -1.0f);
+			GL.Vertex3(1.0f, 1.0f, -1.0f);
+			GL.Vertex3(0.0f, 0.0f, 1.0f);
+			GL.Color4(Color4.LightYellow);
+			GL.Vertex3(1.0f, 1.0f, -1.0f);
+			GL.Vertex3(1.0f, -1.0f, -1.0f);
+			GL.Vertex3(-1.0f, -1.0f, -1.0f);
+			GL.Vertex3(-1.0f, -1.0f, -1.0f);
+			GL.Vertex3(-1.0f, 1.0f, -1.0f);
+			GL.Vertex3(1.0f, 1.0f, -1.0f);
+
+			GL.End();
+		}
+	}
+}

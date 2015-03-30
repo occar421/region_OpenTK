53db37a12c7036ee9672617832a210de5586d574
MasuqaT <occar@hotmail.co.jp>
2015-03-30 21:03:08 +0900

    00-08::Joystick

---
+++ 00-08/00-08_main.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+
+/*
+ * 参照設定 : OpenTK(ver1.1以上) System.Drawing
+ */
+
+namespace OpenTK_Sample
+{
+	class OpenTK_Sample
+	{
+		[STAThread]
+		static int Main()
+		{
+			using (Game window = new Game(GraphicsMode.Default))
+			{
+				window.Run(30.0);
+			}
+			return 0;
+		}
+	}
+
+	class Game : GameWindow
+	{
+		#region Camera__Field
+
+		bool isCameraRotating;		//カメラが回転状態かどうか
+		Vector2 current, previous;	//現在の点、前の点
+		Matrix4 rotate;				//回転行列
+		float zoom;					//拡大度
+		float wheelPrevious;		//マウスホイールの前の状態
+
+		#endregion
+
+		//800x600のウィンドウを作る。タイトルは「0-8:Joystick」
+		public Game(GraphicsMode mode)
+			: base(800, 600, mode, "0-8:Joystick")
+		{
+			#region Camera__Initialize
+
+			isCameraRotating = false;
+			current = Vector2.Zero;
+			previous = Vector2.Zero;
+			rotate = Matrix4.Identity;
+			zoom = 1.0f;
+			wheelPrevious = 0.0f;
+
+			#endregion
+
+			#region Camera__Event
+
+			//マウスボタンが押されると発生するイベント
+			this.Mouse.ButtonDown += (sender, e) =>
+			{
+				//右ボタンが押された場合
+				if (e.Button == MouseButton.Right)
+				{
+					isCameraRotating = true;
+					current = new Vector2(this.Mouse.X, this.Mouse.Y);
+				}
+			};
+
+			//マウスボタンが離されると発生するイベント
+			this.Mouse.ButtonUp += (sender, e) =>
+			{
+				//右ボタンが押された場合
+				if (e.Button == MouseButton.Right)
+				{
+					isCameraRotating = false;
+					previous = Vector2.Zero;
+				}
+			};
+
+			//マウスが動くと発生するイベント
+			this.Mouse.Move += (sender, e) =>
+			{
+				////カメラが回転状態の場合
+				if (isCameraRotating)
+				{
+					previous = current;
+					current = new Vector2(this.Mouse.X, this.Mouse.Y);
+					Vector2 delta = current - previous;
+					delta /= (float)Math.Sqrt(this.Width * this.Width + this.Height * this.Height);
+					float length = delta.Length;
+					if (length > 0.0)
+					{
+						float rad = length * MathHelper.Pi;
+						float theta = (float)Math.Sin(rad) / length;
+						Quaternion after = new Quaternion(delta.Y * theta, delta.X * theta, 0.0f, (float)Math.Cos(rad));
+						rotate = rotate * Matrix4.Rotate(after);
+					}
+				}
+			};
+
+			//マウスホイールが回転すると発生するイベント
+			this.Mouse.WheelChanged += (sender, e) =>
+			{
+				float delta = (float)this.Mouse.Wheel - (float)wheelPrevious;
+
+				zoom *= (float)Math.Pow(1.2, delta);
+
+				//拡大、縮小の制限
+				if (zoom > 2.0f)
+					zoom = 2.0f;
+				if (zoom < 0.5f)
+					zoom = 0.5f;
+				wheelPrevious = this.Mouse.Wheel;
+			};
+
+			#endregion
+
+			VSync = VSyncMode.On;
+		}
+
+		//ウィンドウの起動時に実行される。
+		protected override void OnLoad(EventArgs e)
+		{
+			base.OnLoad(e);
+
+			GL.ClearColor(Color4.Gray);
+			GL.Enable(EnableCap.DepthTest);
+
+			GL.Color4(Color4.White);
+		}
+
+		//ウィンドウのサイズが変更された場合に実行される。
+		protected override void OnResize(EventArgs e)
+		{
+			base.OnResize(e);
+
+			GL.Viewport(ClientRectangle.X, ClientRectangle.Y, ClientRectangle.Width, ClientRectangle.Height);
+			GL.MatrixMode(MatrixMode.Projection);
+			Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView((float)Math.PI / 4, (float)Width / (float)Height, 1.0f, 64.0f);
+			GL.LoadMatrix(ref projection);
+		}
+
+		//画面更新で実行される。
+		protected override void OnUpdateFrame(FrameEventArgs e)
+		{
+			base.OnUpdateFrame(e);
+
+			//Escapeキーで終了
+			if (Keyboard[Key.Escape])
+			{
+				this.Exit();
+			}
+
+			#region Joystick
+			var state = Joystick.GetState(0);
+			if (state.IsConnected)
+			{
+				// Buttons
+				if (state.GetButton(JoystickButton.Button1) == ButtonState.Pressed)
+				{
+					GL.Color4(Color4.IndianRed);
+				}
+				else if (state.IsButtonDown(JoystickButton.Button2))
+				{
+					GL.Color4(Color4.DodgerBlue);
+				}
+
+				// Hat
+				if (state.GetHat(JoystickHat.Hat0).IsUp)
+				{
+					GL.Color4(Color4.White);
+				}
+				else if (state.GetHat(JoystickHat.Hat0).Position.HasFlag(HatPosition.Down))
+				{
+					GL.Color4(Color4.Black);
+				}
+
+				// Axis
+				var x = state.GetAxis(JoystickAxis.Axis0);
+				var y = state.GetAxis(JoystickAxis.Axis1);
+				rotate = rotate * Matrix4.CreateRotationY(0.1f * x) * Matrix4.CreateRotationX(-0.1f * y);
+
+				var zR = state.GetAxis(JoystickAxis.Axis3);
+				zoom = zoom + zR * 0.1f;
+				if (zoom > 2.0f)
+					zoom = 2.0f;
+				if (zoom < 0.5f)
+					zoom = 0.5f;
+			}
+			#endregion
+		}
+
+		//画面描画で実行される。
+		protected override void OnRenderFrame(FrameEventArgs e)
+		{
+			base.OnRenderFrame(e);
+
+			GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+			#region TransFormationMatrix
+
+			Matrix4 modelView = Matrix4.LookAt(Vector3.UnitZ * 10 / zoom, Vector3.Zero, Vector3.UnitY);
+			GL.MatrixMode(MatrixMode.Modelview);
+			GL.LoadMatrix(ref modelView);
+			GL.MultMatrix(ref rotate);
+
+			Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4 / zoom, (float)this.Width / (float)this.Height, 1.0f, 64.0f);
+			GL.MatrixMode(MatrixMode.Projection);
+			GL.LoadMatrix(ref projection);
+
+			#endregion
+
+			GL.Begin(BeginMode.Quads);
+			GL.Vertex3(-3.0f, 3.0f, 0.0f);
+			GL.Vertex3(-3.0f, -3.0f, 0.0f);
+			GL.Vertex3(3.0f, -3.0f, 0.0f);
+			GL.Vertex3(3.0f, 3.0f, 0.0f);
+			GL.End();
+
+			SwapBuffers();
+		}
+	}
+}
